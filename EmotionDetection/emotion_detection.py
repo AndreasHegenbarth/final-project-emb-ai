5e6c6a6cbe7859729016e3c969338c65b3bb7cee
Andreas Hegenbarth <andreas.hegenbarht@gmx.de>
2024-11-13 16:37:33 -0500

    clean up with pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,8 +7,6 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }  
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
     response = requests.post(url, json = myobj, headers=header)  
-
-    
     
     if response.status_code == 200:
         formatted_response = json.loads(response.text)
@@ -21,7 +19,7 @@ def emotion_detector(text_to_analyse):
         fear = emotion['fear']
         sadness = emotion['sadness']
         Keymax = max(zip(emotion.values(), emotion.keys()))[1]
-    # If the response status code is 500, set label and score to None
+
     elif response.status_code == 400:
         anger = ''
         disgust = ''
@@ -29,7 +27,6 @@ def emotion_detector(text_to_analyse):
         fear = ''
         sadness = ''
         Keymax = ''
-   
  
     return {
         'anger': anger, 
@@ -38,4 +35,4 @@ def emotion_detector(text_to_analyse):
         'fear': fear,
         'sadness': sadness,
         'dominant_emotion': Keymax
-        }
+    }
--- server.py
+++ server.py
@@ -1,35 +1,36 @@
+""" Emotion Detection """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 
 app = Flask("Emotion Detection")
 
 @app.route("/")
 def render_index_page():
+    """ Render Template """
+
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_emotion_detector():
-    print('emotion_detector')
-    text_to_analyze = request.args.get('textToAnalyze')
-    print(text_to_analyze)
+    """Get emotion result from input textfield"""
 
+    text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
-    print(response)
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
-    
-    if dominant_emotion is '':
-        return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness':{}.<br /><br /> The dominant emotion is <b>{}</b> ".format(anger, disgust, fear, joy, sadness, dominant_emotion)
 
+    if dominant_emotion != '':
+        return f"""For the given statement, the system response is
+        'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness':{sadness}.
+        <br /><br /> The dominant emotion is <b>{dominant_emotion}</b>
+        """
+
+    return "Invalid input! Try again."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
